4b9898521850f6538e87b07bed66dd9a8b35855f
s3rbug <bugaychuk.sergiy@gmail.com>
2023-03-21 03:48:58 +0200

    better url handling for pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -27,39 +27,36 @@ export const Pagination = ({
 
 	const changeCurrentPage = useCallback(
 		(page: number) => {
-			return () => {
-				setSearchParams({ page: String(page) })
-				setCurrentPage(page)
-			}
+			setSearchParams({ page: String(page) })
+			setCurrentPage(page)
 		},
 		[setCurrentPage, setSearchParams]
 	)
 
-	const resetCurrentPage = useCallback(() => {
-		changeCurrentPage(1)()
-	}, [changeCurrentPage])
-
 	useEffect(() => {
-		if (currentPage !== null) {
+		if (currentPage === null) {
+			dispatch(paginationActions.setCurrentPage({ page: 1 }))
+		}
+
+		if (
+			currentPage === null ||
+			lastPage == null ||
+			currentPage === urlCurrentPage
+		) {
 			return
 		}
 
-		if (isNaN(urlCurrentPage) || urlCurrentPage === 0) {
-			resetCurrentPage()
-		} else {
-			dispatch(paginationActions.setCurrentPage({ page: urlCurrentPage }))
-		}
-	}, [dispatch, urlCurrentPage, currentPage, resetCurrentPage])
-
-	useEffect(() => {
 		if (
-			currentPage !== null &&
-			lastPage !== null &&
-			(currentPage <= 0 || currentPage > lastPage)
+			!isNaN(urlCurrentPage) &&
+			urlCurrentPage >= 1 &&
+			urlCurrentPage <= lastPage
 		) {
-			resetCurrentPage()
+			dispatch(paginationActions.setCurrentPage({ page: urlCurrentPage }))
+		} else {
+			//  should notify
+			changeCurrentPage(1)
 		}
-	}, [currentPage, lastPage, resetCurrentPage])
+	}, [dispatch, urlCurrentPage, currentPage, changeCurrentPage, lastPage])
 
 	if (!currentPage || !lastPage) {
 		return null
@@ -70,7 +67,7 @@ export const Pagination = ({
 	return (
 		<Flex mt={8} gap={2} justifyContent="center">
 			<PaginationLink
-				onClick={changeCurrentPage(currentPage - 1)}
+				onClick={() => changeCurrentPage(currentPage - 1)}
 				isDisabled={currentPage === 1}
 			>
 				Previous
@@ -86,7 +83,7 @@ export const Pagination = ({
 				return (
 					<PaginationLink
 						key={index}
-						onClick={changeCurrentPage(pageNumber)}
+						onClick={() => changeCurrentPage(pageNumber)}
 						isCurrent={currentPage === pageNumber}
 					>
 						{pageNumber}
@@ -94,7 +91,7 @@ export const Pagination = ({
 				)
 			})}
 			<PaginationLink
-				onClick={changeCurrentPage(currentPage + 1)}
+				onClick={() => changeCurrentPage(currentPage + 1)}
 				isDisabled={currentPage === lastPage}
 			>
 				Next
